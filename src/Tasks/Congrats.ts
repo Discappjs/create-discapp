64cf21545361c3a1c207d885dcb3fb9fa8c48d69
Jota <jjunior@protonmail.ch>
2021-02-20 14:39:02 -0300

    Improve congrats message

--- src/Tasks/Congrats.ts
+++ src/Tasks/Congrats.ts
@@ -1,9 +1,10 @@
 import chalk from 'chalk'
+
 import { state } from '../state'
 import { Task } from './Task'
 
 export class Congrats implements Task {
-  public description = ''
+  public description = 'Ready'
 
   public execute() {
     console.log(
@@ -12,7 +13,7 @@ export class Congrats implements Task {
       )}\n`
     )
 
-    console.log('Get started by running:')
+    console.log('Get started by running:\n')
     console.log(`    ${chalk.blue(`cd ${state.projectName}`)}`)
     console.log(`    ${state.client} dev`)
   }
